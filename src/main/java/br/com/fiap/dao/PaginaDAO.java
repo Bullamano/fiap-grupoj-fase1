001e3186b92ab7f673713acb47f7f559cfc4c2e4
Allan <allanblamano@gmail.com>
2022-05-08 14:58:52 -0300

    Testes para Pagina

--- src/main/java/br/com/fiap/dao/PaginaDAO.java
+++ src/main/java/br/com/fiap/dao/PaginaDAO.java
@@ -1,12 +1,91 @@
 package br.com.fiap.dao;
 
+import java.util.List;
+import br.com.fiap.needhelpapp.model.Categoria;
 import br.com.fiap.needhelpapp.model.Pagina;
 import jakarta.persistence.EntityManager;
 
+/**
+ * Classe DAO para persistência de objetos Pagina
+ */
 public class PaginaDAO extends GenericDAO<Pagina, Integer> {
 	
+	/**
+	 * Construtor
+	 * @param entityManager Entity Manager
+	 */
 	public PaginaDAO(EntityManager entityManager) {
 		super(entityManager);
 	}
 
+	/**
+	 * Pesquisa de Pagina por nome
+	 * @param nome String com o nome desejado para a pesquisa
+	 * @return uma Lista de paginas com o nome desejado
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Pagina> getByName(String nome) 
+	{		 
+		return (List<Pagina>)this.em.createQuery(
+				"select e from Pagina e where e.nome = :nome"
+				).setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	/**
+	 * Pesquisa de Pagina por um nome único
+	 * @param nome String com o nome desejado para a pesquisa
+	 * @return Um único resultado com o nome desejado
+	 */
+	public Pagina getByNameUnique(String nome) 
+	{		 
+		return (Pagina)this.em.createQuery(
+				"select e from Pagina e where e.nome = :nome"
+				).setParameter("nome", nome)
+				.getSingleResult();
+	}
+	
+	/**
+	 * Pesquisa de Pagina por um nome parcial
+	 * @param nome String com o nome (parcial) desejado para a pesquisa
+	 * @return uma Lista com as paginas cujos nomes possuem a string desejada
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Pagina> getByNamePartial(String nome) 
+	{		 
+		return (List<Pagina>)this.em.createQuery(
+				"select e from Pagina e where e.nome like concat('%', :nome,'%')"
+				).setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	/**
+	 * Pesquisa de Pagina de acordo com uma categoria
+	 * @param categoria Objeto Categoria
+	 * @return Lista de paginas relacionadas a uma categoria
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Pagina> getByCategoria(Categoria categoria) 
+	{		 
+		int categoriaId = categoria.getId();
+		return (List<Pagina>)this.em.createQuery(
+				"select e from Pagina e where e.categoria = :categoriaId"
+				).setParameter("categoriaId", categoriaId)
+				.getResultList();
+	}
+	
+	/**
+	 * Pesquisa de Pagina de acordo com uma categoria
+	 * @param categoriaId ID de uma categoria
+	 * @return Lista de paginas relacionadas a uma categoria
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Pagina> getByCategoria(Integer categoriaId) 
+	{		 
+		return (List<Pagina>)this.em.createQuery(
+				"select e from Pagina e where e.categoria = :categoriaId"
+				).setParameter("categoriaId", categoriaId)
+				.getResultList();
+	}
+	
 }
---
+++ src/main/java/br/com/fiap/needhelpapp/tests/NHAFPaginaTests.java
@@ -0,0 +1,120 @@
+package br.com.fiap.needhelpapp.tests;
+
+import java.util.List;
+import br.com.fiap.dao.CategoriaDAO;
+import br.com.fiap.dao.PaginaDAO;
+import br.com.fiap.needhelpapp.model.Categoria;
+import br.com.fiap.needhelpapp.model.Pagina;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+public class NHAFPaginaTests {
+	
+public static void main(String[] args) {
+		
+		EntityManager entityManager = null;
+        try 
+        {
+        	//Entity Manager
+        	entityManager =  Persistence.
+                createEntityManagerFactory("fiap-grupoj-atividade-fase1").
+                createEntityManager();
+        	
+        	//DAO para os objetos Categoria e Pagina
+        	CategoriaDAO categoriaDAO = new CategoriaDAO(entityManager);
+        	PaginaDAO paginaDAO = new PaginaDAO(entityManager);
+
+        	/**
+        	 * Listagem de todas as entradas no banco de dados
+        	 */
+            List<Pagina> testePag = paginaDAO.listar();
+            System.out.println("\nListagem de todas as páginas:");
+            for(Pagina pag : testePag)
+            {
+            	System.out.println(pag);
+            }
+            
+            /**
+             * Pesquisa por uma string nome de uma pagina (retorno lista)
+             */            
+            System.out.println("\nPesquisa por nome (lista):");
+            System.out.println(paginaDAO.getByName("Lasagna"));
+            
+            /**
+             * Pesquisa por uma string nome de uma pagina (retorno único)
+             */            
+            System.out.println("\nPesquisa por nome (único):");
+            System.out.println(paginaDAO.getByNameUnique("Nós de gravata"));
+            
+            /**
+             * Pesquisa por uma string parcial do nome de uma página
+             */            
+            System.out.println("\nPesquisa por nome (parcial):");
+            System.out.println(paginaDAO.getByNamePartial("Como"));
+            
+            /**
+             * Pesquisa por um ID de uma página
+             */            
+            System.out.println("\nPesquisa por ID:");
+            System.out.println(paginaDAO.recuperar(1));
+            
+            /**
+             * Criação de objeto no banco
+             */
+            entityManager.getTransaction().begin();
+            
+            Pagina paginaNova = new Pagina();
+            paginaNova.setNome("Tarefa inútil");
+            Categoria categPagNova = categoriaDAO.recuperar(1);
+            paginaNova.setCategoria(categPagNova);
+            
+            paginaDAO.salvar(paginaNova);
+            
+            entityManager.getTransaction().commit();
+            
+            System.out.println("\nPesquisando a nova página:");
+            System.out.println(paginaDAO.getByNameUnique(paginaNova.getNome()));
+            List<Pagina> pagPostInsert = paginaDAO.listar();
+            System.out.println("\nListagem das páginas após o insert:");
+            for(Pagina pag : pagPostInsert)
+            {
+            	System.out.println(pag);
+            }
+            
+            /**
+             * Deletando o objeto criado acima
+             */
+            System.out.println("\nDeletando a página criada anteriormente...");
+            entityManager.getTransaction().begin();
+            
+            paginaDAO.excluir(paginaNova.getId());
+            
+            entityManager.getTransaction().commit();
+            
+            List<Pagina> pagPostDelete = paginaDAO.listar();
+            System.out.println("\nListagem das páginas após o delete:");
+            for(Pagina pag : pagPostDelete)
+            {
+            	System.out.println(pag);
+            }
+        } 
+        catch (Exception e)
+        {
+			if (entityManager != null && entityManager.getTransaction().isActive())
+			{
+				entityManager.getTransaction().rollback();
+		    }
+			e.printStackTrace();
+        }
+        finally
+        {
+        	if (entityManager != null)
+        	{
+     		   entityManager.close();
+     	    }
+        }   
+        
+	   System.exit(0);
+	}
+
+}

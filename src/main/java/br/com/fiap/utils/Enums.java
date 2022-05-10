15fe32c14483318a95992b5f92712e84179a9a7f
Allan <allanblamano@gmail.com>
2022-05-10 19:44:43 -0300

    Comentários

--- src/main/java/br/com/fiap/utils/Enums.java
+++ src/main/java/br/com/fiap/utils/Enums.java
@@ -1,11 +1,21 @@
 package br.com.fiap.utils;
 
+/**
+ * Classe de Enums para o projeto
+ */
 public class Enums {
 	
+	/**
+	 * Ordem na qual uma pesquisa ao banco deve ter seus
+	 * registros retornados
+	 */
 	public static enum order{
 		asc, desc, irrelevant
 	}
 	
+	/**
+	 * Tipo de link para os Recursos da aplicação
+	 */
 	public static enum linkType{
 		leitura, imagem, video
 	}
